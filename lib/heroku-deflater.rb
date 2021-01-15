afd3c1cb73bd872e0e38a9829f0d43203c45e4d8
Daniel Greenlaw <daniel@shirtspace.com>
2021-01-15 15:39:53 -0800

    Add Rails 6.1 version check method

--- lib/heroku-deflater.rb
+++ lib/heroku-deflater.rb
@@ -1,6 +1,10 @@
 require 'heroku-deflater/railtie'
 
 module HerokuDeflater
+  def self.rails_version_6_1?
+    Rails::VERSION::MAJOR >= 6 && Rails::VERSION::MINOR >= 1
+  end
+
   def self.rails_version_5?
     Rails::VERSION::MAJOR >= 5
   end
